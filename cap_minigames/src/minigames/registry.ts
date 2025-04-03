61575e34cd2bc488c0aec8103d5c045aaafcd638
TURR3T5 <casper.truberg@outlook.dk>
2025-04-03 21:29:27 +0200

    removed some of them

--- cap_minigames/src/minigames/registry.ts
+++ cap_minigames/src/minigames/registry.ts
@@ -1,16 +1,12 @@
 import { MinigameRegistry } from '../core/types';
 import TerminalHacking from './TerminalHacking';
-import QuantumDecryption from './QuantumDecryption';
 import SignalTriangulation from './SignalTriangulation';
 import MemoryFragmentSequencing from './MemoryFragmentSequencing';
-import BiometricOverride from './BiometricOverride';
 import BlockchainVerification from './BlockchainVerification';
 
 export const minigames: MinigameRegistry = {
   terminalHacking: TerminalHacking,
-  quantumDecryption: QuantumDecryption,
   signalTriangulation: SignalTriangulation,
   memoryFragmentSequencing: MemoryFragmentSequencing,
-  biometricOverride: BiometricOverride,
   blockchainVerification: BlockchainVerification,
 };
